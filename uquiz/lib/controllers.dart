0336eda81ed0a72d80eb955b226a011c30437b99
pongsapat64 <pongsapat.ch.64@ubu.ac.th>
2024-09-23 12:15:26 +0700

    change route add GetX but still not work

---
+++ uquiz/lib/controllers.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pocketbase/pocketbase.dart';
+import 'package:uquiz/members.dart';
+import 'models.dart';
+import 'package:uquiz/shopping.dart';
+
+class UQuizController extends GetxController {
+  var appName = 'uQuiz'.obs;
+  var memberCount = 0.obs;
+  var isAdmin = false.obs;
+  var memberList = <Member>[].obs;
+  final pb = PocketBase('http://127.0.0.1:8090');
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future<void> authen(String email, String password) async {
+      final res = pb.admins.authWithPassword(email, password);
+      isAdmin = true.obs;
+      if (pb.authStore.isValid) {
+        print('admin logged in: ${email}');
+        Get.to(()=> const MemberListPage());
+      } else {
+        final res = pb.collection('users').authWithPassword(email, password);
+        isAdmin = false.obs;
+        if (pb.authStore.isValid) {
+          print('user logged in: ${email}');
+          Get.to(()=> const Shopping());
+        }
+      }
+    }
+  }
--- uquiz/lib/login.dart
+++ uquiz/lib/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uquiz/controllers.dart';
 import 'package:uquiz/shopping.dart';
+
+import 'members.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,54 +13,81 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // var String name = "Paul";
   final _formKey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
+  // final emailController = TextEditingController();
+  // final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final ctrl = Get.find<UQuizController>();
     return Scaffold(
-        body: Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("Login", style: TextStyle(fontSize: 40)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: emailController,
-              validator: (value) {
-                return null;
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Login', style: TextStyle(fontSize: 40))),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: ctrl.emailController,
+                validator: (value) {
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("password"),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ElevatedButton(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                obscureText: true,
+                controller: ctrl.passwordController,
+                validator: (value) {
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
-                    // Navigator.push(context);
-                    print('email: ${emailController.text}');
-                    Navigator.push<void>(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => const Shopping())
+                    // Perform login logic here
+                    print('email: ${ctrl.emailController.text}');
+                    print('password: ${ctrl.passwordController.text}');
+                    //Navigator.pop(context);
+                    ctrl.authen(
+                      ctrl.emailController.text,
+                      ctrl.passwordController.text,
                     );
+                    // final email = ctrl.emailController.text;
+                    // if (email == 'admin@ubu.ac.th') {
                   }
                 },
-                child: const Text("Submit")),
-          )
-        ],
+                child: const Text('Submit'),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  void to(BuildContext context, Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => page,
+      ),
+    );
   }
 }
